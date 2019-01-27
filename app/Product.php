a5fae3793396425b7c391104c5e4fbfa5ccb7585
Oleksii Kruhlyk <alexkruglik81@gmail.com>
2019-01-27 13:36:58 +0200

    Seeders

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['title', 'slug', 'description', 'price', 'barcode', 'stock', 'cover'];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function gallery()
+    {
+        return$this->hasOne(Gallery::class);
+    }
 }
